a397774f0dec45a811f3ff107360004150875de5
sm-sadi <63817831+sm-sadi@users.noreply.github.com>
2021-01-04 21:22:36 +0100

    navbar and footer fix with dynamic content

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-let projectName =" team project for school"
+
+// making navbar visible when scrolling  
+const navbar = document.getElementById("nav")
+
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    // console.log(scroll)
+    if(scroll > 250){
+        navbar.classList.add("sticky-top")
+        navbar.classList.add("shadow-sm")
+    }else{
+        navbar.classList.remove("sticky-top")
+        navbar.classList.remove("shadow-sm")
+    }
+    
+});
+
+// setting up dymanic year 
+const Currentyear = document.getElementById("year");
+
+const currentDate =  new Date()
+const yearNow = currentDate.getFullYear()
+
+Currentyear.innerHTML = yearNow
